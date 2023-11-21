6dbae4c2b72997c837ddc07b187c8b1f0432e2ff
Ivan <94634131+veetsok@users.noreply.github.com>
2023-11-21 10:34:02 +0400

    update Fonts

--- src/user.InterfaceLayer/constants/fonts/index.ts
+++ src/user.InterfaceLayer/constants/fonts/index.ts
@@ -1,5 +1,8 @@
 enum FontsEnum {
-  AvenirNextCyr = "AvenirNextCyr-Regular",
+  InterRegular = "Inter-Regular",
+  InterMedium = "Inter-Medium",
+  InterBold = "Inter-Bold",
+  InterBlack = "Inter-Black",
 }
 
 export default FontsEnum;
